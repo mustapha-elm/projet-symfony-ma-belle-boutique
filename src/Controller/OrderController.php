2c5285156638f5b322b3c1388fd81caf5a62318c
mustapha <mustapha.elm@hotmail.fr>
2021-10-06 21:56:28 +0200

    47. Tunnel d'achat : Stocker les informations de la commande en base

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use DateTime;
+use App\Classe\Cart;
+use App\Entity\Order;
 use App\Form\OrderType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\OrderDetails;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/commande", name="order")
      */
-    public function index(): Response
+    public function index(Cart $cart, Request $request): Response
     {
 
         if (!$this->getUser()->getAdresses()->getValues()) {
@@ -24,7 +38,74 @@ class OrderController extends AbstractController
         ]);
 
         return $this->render('order/index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'cart' => $cart->getFull()
         ]);
+    }
+
+
+    /**
+     * @Route("/commande/recapitulatif", name="order-recap", methods={"POST"})
+     */
+    public function add(Cart $cart, Request $request): Response
+    {
+
+
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $date = new DateTime();
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('addresses')->getData();
+            $delivery_content = $delivery->getFirstname() . ' ' . $delivery->getLastname();
+            $delivery_content .= '<br/>' . $delivery->getPhone();
+            if ($delivery->getCompany()) {
+                $delivery_content .= '<br/>' . $delivery->getCompany();
+            }
+            $delivery_content .= '<br/>' . $delivery->getAdress();
+            $delivery_content .= '<br/>' . $delivery->getPostal() . ' ' . $delivery->getCity();
+            $delivery_content .= '<br/>' . $delivery->getCountry();
+
+
+
+            //enregistrer ma commande order()
+            $order = new Order();
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->setIsPaid(0);
+
+            $this->entityManager->persist($order);
+
+            //enregistrer mes produits erderDetails()
+
+            foreach ($cart->getFull() as $product) {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($product['product']->getName());
+                $orderDetails->setQuantity($product['quantity']);
+                $orderDetails->setPrice($product['product']->getPrice());
+                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
+                $this->entityManager->persist($orderDetails);
+            }
+
+            //$this->entityManager->flush();
+
+            return $this->render('order/add.html.twig', [
+                'cart' => $cart->getFull(),
+                'carrier' => $carriers,
+                'delivery' => $delivery_content
+            ]);
+        }
+
+        return $this->redirectToRoute('cart');
     }
 }
